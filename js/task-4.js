61e956ab5f04cf1497190c580baf6e4965a7ba01
Taisiia713 <taisia197@ukr.net>
2024-02-19 21:34:41 +0100

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const email = this.elements.email.value.trim();
+  const password = this.elements.password.value.trim();
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    this.reset();
+  }
+});
