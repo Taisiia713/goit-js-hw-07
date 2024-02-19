7a68ef27fc47d7cbe9822b9d709d67c1bfd73a65
Taisiia713 <taisia197@ukr.net>
2024-02-19 21:52:53 +0100

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const span = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", function () {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  span.textContent = color;
+});
